a418d2cca116230de7ad8838de1e876ddc821fa1
ClaudeAlves <claudeuw@hotmail.com>
2019-11-06 17:24:31 +0100

    gang gang

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,29 +4,53 @@ import java.awt.*;
 public class MainFrame extends JFrame{
     private JPanel mainPanel = new JPanel();
     private JPanel imagePanel = new JPanel();
-    private JPanel annotationPanel = new JPanel();
+    private JScrollPane annotationPanel = new JScrollPane();
     private JMenuBar menuBar = new JMenuBar();
     private JMenuItem m1 = new JMenuItem();
     private JMenuItem m2 = new JMenuItem();
+    private JMenuItem m3 = new JMenuItem();
+    private JMenuItem m4 = new JMenuItem();
+    private JMenuItem m5 = new JMenuItem();
     private JMenu menu = new JMenu();
+    private JButton newAnnotation = new JButton();
 
     public MainFrame(){
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        this.setLayout(new BoxLayout());
 
+        m1.setText("Import File");
+        m2.setText("Export File");
+        m3.setText("Save");
+        m4.setText("Save As");
+        m5.setText("Exit");
+        menu.setText("Menu");
+        JTextArea textArea = new JTextArea(5,40);
+        textArea.add(new JLabel());
+        annotationPanel.setBackground(new Color(52,62,69));
+        annotationPanel.add(textArea);
+
+        newAnnotation.setText("New Annotation");
         mainPanel.setPreferredSize(new Dimension(1920, 1080));
+        this.setMaximumSize(new Dimension(800,600));
         mainPanel.setMinimumSize(new Dimension(800,600));
         mainPanel.setBackground(new Color(62,72,79));
+        mainPanel.setLayout(new BorderLayout());
         this.setContentPane(mainPanel);
+
 
         menu.add(m1);
         menu.add(m2);
+        menu.add(m3);
+        menu.add(m4);
+        menu.add(m5);
         menuBar.add(menu);
-        mainPanel.add(menuBar);
+        menuBar.add(newAnnotation);
+        mainPanel.add(annotationPanel, BorderLayout.EAST);
+        menuBar.setBackground(new Color(70,80,87));
+        mainPanel.add(menuBar, BorderLayout.NORTH);
 
         imagePanel.setPreferredSize(new Dimension(800,600));
-        imagePanel.setBackground(new Color(255, 0, 0));
+        imagePanel.setBackground(new Color(62,72,70));
         mainPanel.add(imagePanel);
         this.setVisible(true);
     }
