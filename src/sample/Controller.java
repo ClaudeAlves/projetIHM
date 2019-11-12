750698fd0a29e16efe4e1b8a84e7ded328b92a21
Guillaume Vetter <guillaume.vetter@heig-vd.ch>
2019-11-12 19:40:34 +0100

    Adding viewList and correct annotation selection

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,29 +1,41 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.awt.Point;
+import java.awt.*;
 
 import javafx.scene.image.Image;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
 public class Controller {
     public ImageView imageView;
     public Pane drawingPanel;
+    public ScrollPane labelPane;
+    public ColorPicker colorPicker;
+    public ListView listLabel;
+    public Pane mainPanel;
     private Point startingPoint = new Point();
     private Point endingPoint = new Point();
-    private Rectangle selectionRectangle;
+    private Rectangle selectionRectangle = new Rectangle();
     private Stage stage = new Stage();
     private File currentFile = null;
+    private ArrayList<TextField> labels = new ArrayList<>();
+    private boolean newAnnotation;
 
     public void exit(ActionEvent actionEvent) {
         System.exit(0);
@@ -81,14 +93,44 @@ public class Controller {
         }
     }
 
+    public void setNewAnnotation(){
+        newAnnotation = true;
+    }
+
     public void mouseClicked(MouseEvent mouseEvent){
-        startingPoint = new Point((int)mouseEvent.getX(), (int)mouseEvent.getY());
-        selectionRectangle.setStroke(Color.color(255, 0 ,0));
-        drawingPanel.getChildren().add(selectionRectangle);
+        if(newAnnotation) {
+            startingPoint = new Point((int) mouseEvent.getX(), (int) mouseEvent.getY());
+            selectionRectangle = new Rectangle();
+            selectionRectangle.setX(startingPoint.x);
+            selectionRectangle.setY(startingPoint.y);
+            selectionRectangle.setStroke(colorPicker.getValue());
+            selectionRectangle.setFill(null);
+            drawingPanel.getChildren().add(selectionRectangle);
+        }
     }
 
     public void mouseDragged(MouseEvent mouseEvent){
-        endingPoint = endingPoint.getLocation();
-        selectionRectangle = new Rectangle(startingPoint.x, startingPoint.y, endingPoint.x - startingPoint.x, endingPoint.y - startingPoint.y);
+        if(newAnnotation) {
+
+            endingPoint = new Point((int) mouseEvent.getX(), (int) mouseEvent.getY());
+
+            selectionRectangle.setWidth(Math.abs(endingPoint.x >= imageView.getFitWidth() ? imageView.getFitWidth() - startingPoint.x : mouseEvent.getX() - startingPoint.x));
+            selectionRectangle.setHeight(Math.abs(endingPoint.y >= imageView.getFitHeight() ? imageView.getFitHeight() - startingPoint.y : mouseEvent.getY() - startingPoint.y));
+
+            selectionRectangle.setX(Math.min(startingPoint.x, endingPoint.x >= 0 ? mouseEvent.getX() : 0));
+            selectionRectangle.setY(Math.min(startingPoint.y, endingPoint.y >= 0 ? mouseEvent.getY() : 0));
+        }
+
+    }
+
+    public void mouseReleased(MouseEvent mouseEvent){
+        if(newAnnotation) {
+            startingPoint = null;
+            endingPoint = null;
+            newAnnotation = false;
+            TextField label = new TextField();
+            listLabel.getItems().add(label);
+            label.requestFocus();
+        }
     }
 }
